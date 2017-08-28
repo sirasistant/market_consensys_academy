b3f93c9161ef4ab5128a75f2f2e8208f580277a3
sirasistant <sirasistant@gmail.com>
2017-08-28 03:10:35 +0200

    Updated the tests

--- test/market.js
+++ test/market.js
@@ -1,4 +1,5 @@
 var Market = artifacts.require("./Market.sol");
+web3.eth.expectedExceptionPromise = require("../app/lib/expectedExceptionPromise.js");
 
 const promisify = (inner) =>
   new Promise((resolve, reject) =>
@@ -20,75 +21,84 @@ contract('Market', function (accounts) {
     instance = await Market.new(fee);
   });
 
-  it("should set the owner as admin", async () => {
-    var isAdmin = await instance.isAdmin(accounts[0]);
-    assert.equal(isAdmin, true);
+  describe("Lifecycle", () => {
+    it("should set the owner as admin", async () => {
+      var isAdmin = await instance.isAdmin(accounts[0]);
+      assert.equal(isAdmin, true);
+    });
+
+    it("should kill itself", async () => {
+      await instance.kill({ from: accounts[0] });
+      var code = web3.eth.getCode(instance.address)
+      assert.equal(code, "0x0", "Did not kill itself");
+    });
   });
 
-  it("should add and delete sellers", async () => {
-    await instance.addSeller(accounts[0], { from: accounts[0] });
-    var isSeller = await instance.isSeller(accounts[0]);
-    assert.equal(isSeller, true);
-    await instance.deleteSeller(accounts[0], { from: accounts[0] });
-    isSeller = await instance.isSeller(accounts[0]);
-    assert.equal(isSeller, false);
+  describe("Users", () => {
+    it("should add and delete sellers", async () => {
+      await instance.addSeller(accounts[0], { from: accounts[0] });
+      var isSeller = await instance.isSeller(accounts[0]);
+      assert.equal(isSeller, true);
+      await instance.deleteSeller(accounts[0], { from: accounts[0] });
+      isSeller = await instance.isSeller(accounts[0]);
+      assert.equal(isSeller, false);
+    });
+
+    it("should add and delete admins", async () => {
+      await instance.addAdmin(accounts[1], { from: accounts[0] });
+      var isAdmin = await instance.isAdmin(accounts[1]);
+      assert.equal(isAdmin, true);
+      await instance.deleteAdmin(accounts[1], { from: accounts[0] });
+      isAdmin = await instance.isAdmin(accounts[1]);
+      assert.equal(isAdmin, false);
+    });
   });
 
-  it("should add and delete admins", async () => {
-    await instance.addAdmin(accounts[1], { from: accounts[0] });
-    var isAdmin = await instance.isAdmin(accounts[1]);
-    assert.equal(isAdmin, true);
-    await instance.deleteAdmin(accounts[1], { from: accounts[0] });
-    isAdmin = await instance.isAdmin(accounts[1]);
-    assert.equal(isAdmin, false);
-  });
-
-  it("should manage products", async () => {
-    var price = 100;
-    var overpay = 5;
-    await instance.addSeller(accounts[2], { from: accounts[0] });
-    await instance.addProduct(price, 2, "Producto", { from: accounts[2] });
-    var productsCount = await instance.getProductsCount();
-    assert.equal("1", productsCount.toString(10), "Did not add the product correctly");
-    await instance.buy(0, { from: accounts[1], value: price + overpay });
-    var sellerBalance = await instance.balances(accounts[2]);
-    assert.equal(sellerBalance.toString(10), "" + (price - fee), "Did not update the amount of the seller correctly");
-    var ownerBalance = await instance.balances(accounts[0]);
-    assert.equal(ownerBalance.toString(10), "" + (fee + overpay), "Did not update the amount of the owner correctly");
-  });
-
-  it("should manage money", async () => {
+  describe("Manage products", () => {
     var price = 100;
     var overpay = 5;
     var gasPrice = 40000000;
-    var ownerAccountBalance, sellerAccountBalance;
+    var owner = accounts[0];
+    var buyer = accounts[1];
+    var seller = accounts[2];
 
-    await instance.addSeller(accounts[2], { from: accounts[0] });
+    beforeEach(async () => {
+      await instance.addSeller(seller, { from: owner });
+      await instance.addProduct(price, 2, "Producto", { from: seller });
+    });
 
-    await instance.addProduct(price, 2, "Producto", { from: accounts[2] });
+    it("Should add products", async () => {
+      var productsCount = await instance.getProductsCount();
+      assert.equal("1", productsCount.toString(10), "Did not add the product correctly");
+    });
 
-    var productsCount = await instance.getProductsCount();
+    it("Should allow buying products", async () => {
+      await instance.buy(0, { from: buyer, value: price });
+      var sellerBalance = await instance.balances(seller);
+      assert.equal(sellerBalance.toString(10), "" + (price - fee), "Did not update the amount of the seller correctly");
+      var ownerBalance = await instance.balances(owner);
+      assert.equal(ownerBalance.toString(10), "" + (fee ), "Did not update the amount of the owner correctly");
+    });
 
-    await instance.buy(0, { from: accounts[1], value: price + overpay });
+    it("Shouldn't allow buying products paying less", async () => {
+      await web3.eth.expectedExceptionPromise(() => instance.buy(0, { from: owner, gas: 3000000 }), 3000000);
+    });
 
-    var accountBalances = await Promise.all([accounts[0], accounts[2]].map(account => getBalance(account)));
-    ownerAccountBalance = accountBalances[0];
-    sellerAccountBalance = accountBalances[1];
+    it("Should allow retrieving money", async () => {
+      await instance.buy(0, { from: buyer, value: price + overpay });
 
-    var receipts = await Promise.all([accounts[0], accounts[2]].map(account => instance.withdraw({ from: account, gasPrice: gasPrice })));
-    ownerAccountBalance = ownerAccountBalance.minus(receipts[0].receipt.gasUsed * gasPrice);
-    sellerAccountBalance = sellerAccountBalance.minus(receipts[1].receipt.gasUsed * gasPrice);
-    accountBalances = await Promise.all([accounts[0], accounts[2]].map(account => getBalance(account)));
+      var accountBalances = await Promise.all([owner, seller].map(account => getBalance(account)));
+      var ownerAccountBalance = accountBalances[0];
+      var sellerAccountBalance = accountBalances[1];
 
-    assert.equal(accountBalances[0].toString(10), ownerAccountBalance.add(fee + overpay).toString(10), "Did not retrieve the amount of the owner correctly");
-    assert.equal(accountBalances[1].toString(10), sellerAccountBalance.add(price - fee).toString(10), "Did not retrieve the amount of the seller correctly");
+      var receipts = await Promise.all([owner, seller].map(account => instance.withdraw({ from: account, gasPrice: gasPrice })));
+      ownerAccountBalance = ownerAccountBalance.minus(receipts[0].receipt.gasUsed * gasPrice);
+      sellerAccountBalance = sellerAccountBalance.minus(receipts[1].receipt.gasUsed * gasPrice);
+      accountBalances = await Promise.all([owner, seller].map(account => getBalance(account)));
+
+      assert.equal(accountBalances[0].toString(10), ownerAccountBalance.add(fee + overpay).toString(10), "Did not retrieve the amount of the owner correctly");
+      assert.equal(accountBalances[1].toString(10), sellerAccountBalance.add(price - fee).toString(10), "Did not retrieve the amount of the seller correctly");
+    });
   });
-
-  it("should kill itself", async () => {
-    await instance.kill({ from: accounts[0] });
-    var code = web3.eth.getCode(instance.address)
-    assert.equal(code, "0x0", "Did not kill itself");
-  });
-
 
 });
