4793857932311d023a2a60431e017458e17f2ed5
sirasistant <sirasistant@gmail.com>
2017-08-29 02:18:43 +0200

    Separated contracts

---
+++ contracts/AdminManager.sol
@@ -0,0 +1,71 @@
+pragma solidity ^0.4.6;
+
+contract AdminManager{
+    event LogAddAdmin(address account);
+    event LogRemoveAdmin(address account);
+    
+    struct AdminStruct{
+        bool isIndeed;
+        uint index;
+    }
+    
+    mapping(address=>AdminStruct) private adminStructs;
+    
+    address[] private admins;
+    
+    function isAdmin(address account)
+    public
+    constant
+    returns (bool isIndeed){
+        return adminStructs[account].isIndeed;
+    }
+    
+    function insertAdminInternal(address account)
+    internal
+    returns(bool success){
+        require(!isAdmin(account));
+        uint index = admins.push(account)-1;
+        AdminStruct memory adminStruct;
+        adminStruct.isIndeed = true;
+        adminStruct.index = index;
+        adminStructs[account] = adminStruct;
+        
+        LogAddAdmin(account);
+        return true;
+    }
+    
+    function removeAdminInternal(address account)
+    internal
+    returns(bool success){
+        require(isAdmin(account));
+        adminStructs[account].isIndeed = false;
+        uint index = adminStructs[account].index;
+        delete admins[index];
+        if(index!=admins.length-1){
+            //Move the item
+            address toMove = admins[admins.length-1];
+            adminStructs[toMove].index = index;
+            admins[index]= toMove;
+        }
+        admins.length--;
+        
+        LogRemoveAdmin(account);
+        return true;
+    }
+    
+    function getAdminsCount()
+    public
+    constant
+    returns (uint amount){
+        return admins.length;
+    }
+    
+    function getAdminAt(uint index)
+    public
+    constant
+    returns (address admin){
+        require(admins.length>index);
+        return admins[index];
+    }
+    
+}
